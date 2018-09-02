212abc8d255c38d1df606a0fe4c582a402046f0e
sbx_user1059 <sbx_user1059@ip-10-128-71-24.eu-west-2.compute.internal>
2018-09-02 13:52:01 +0000

    Sun Sep 02 13:52:01 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,5 +1,10 @@
 package befaster.solutions.CHK;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CheckoutSolution
 {
     public Integer checkout(String skus)
@@ -29,5 +34,18 @@ public class CheckoutSolution
         return  totalPrice;
     }
 
-    
+    private List<String> getItems(String skus)
+    {
+        List<String> items = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("\\d*[A-D]");
+        Matcher matcher = pattern.matcher(skus);
+
+        while (matcher.find())
+        {
+            items.add(matcher.group());
+        }
+
+        return items;
+    }
 }
