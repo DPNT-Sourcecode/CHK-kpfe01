6051630acdceadaa3f6213300aad3048e78c443b
sbx_user1059 <sbx_user1059@ip-10-128-71-24.eu-west-2.compute.internal>
2018-09-02 13:25:38 +0000

    Sun Sep 02 13:25:38 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,11 +1,59 @@
 package befaster.solutions.FIZ;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FizzBuzzSolution {
+public class FizzBuzzSolution
+{
+    public String fizzBuzz(Integer number)
+    {
+        String numberString = "" + number;
+        List<String> words = new ArrayList<>();
 
-    public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        if (number % 3 == 0 || numberString.contains("3"))
+        {
+            words.add("fizz");
+        }
+
+        if (number % 5 == 0 || numberString.contains("5"))
+        {
+            words.add("buzz");
+        }
+
+        if (isDeluxe(number))
+        {
+            if (number % 2 == 1)
+            {
+                words.add("fake");
+            }
+
+            words.add("deluxe");
+        }
+
+        if (words.size() > 0)
+        {
+            return String.join(" ", words);
+        }
+        else
+        {
+            return numberString;
+        }
     }
 
+    private boolean isDeluxe(int number)
+    {
+        String numberString = "" + number;
+
+        if (number % 3 == 0 && numberString.contains("3"))
+        {
+            return true;
+        }
+
+        if (number % 5 == 0 && numberString.contains("5"))
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
---
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -0,0 +1,112 @@
+package befaster.solutions.FIZ;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzSolutionTest
+{
+	private FizzBuzzSolution solution = new FizzBuzzSolution();
+
+	@Test
+	public void oneReturnsOne()
+	{
+		assertEquals("1", solution.fizzBuzz(1));
+	}
+
+	@Test
+	public void twoReturnsTwo()
+	{
+		assertEquals("2", solution.fizzBuzz(2));
+	}
+
+	@Test
+	public void threeReturnsFizzFakeDeluxe()
+	{
+		assertEquals("fizz fake deluxe", solution.fizzBuzz(3));
+	}
+
+	@Test
+	public void fourReturnsFour()
+	{
+		assertEquals("4", solution.fizzBuzz(4));
+	}
+
+	@Test
+	public void fiveReturnsBuzzFakeDeluxe()
+	{
+		assertEquals("buzz fake deluxe", solution.fizzBuzz(5));
+	}
+
+	@Test
+	public void sixReturnsFizz()
+	{
+		assertEquals("fizz", solution.fizzBuzz(6));
+	}
+
+	@Test
+	public void tenReturnsBuzz()
+	{
+		assertEquals("buzz", solution.fizzBuzz(10));
+	}
+
+	@Test
+	public void elevenReturnsEleven()
+	{
+		assertEquals("11", solution.fizzBuzz(11));
+	}
+
+	@Test
+	public void thirteenReturnsFizz()
+	{
+		assertEquals("fizz", solution.fizzBuzz(13));
+	}
+
+	@Test
+	public void fifteenReturnsFizzBuzzFakeDeluxe()
+	{
+		assertEquals("fizz buzz fake deluxe", solution.fizzBuzz(15));
+	}
+
+	@Test
+	public void twentyTwoReturnsTwentyTwo()
+	{
+		assertEquals("22", solution.fizzBuzz(22));
+	}
+
+	@Test
+	public void thirtyReturnsFizzBuzz()
+	{
+		assertEquals("fizz buzz deluxe", solution.fizzBuzz(30));
+	}
+
+	@Test
+	public void thirtyThreeReturnsFizzFakeDeluxe()
+	{
+		assertEquals("fizz fake deluxe", solution.fizzBuzz(33));
+	}
+
+	@Test
+	public void fiftyOneReturnsFizzBuzz()
+	{
+		assertEquals("fizz buzz", solution.fizzBuzz(51));
+	}
+
+	@Test
+	public void fiftyTwoReturnsBuzz()
+	{
+		assertEquals("buzz", solution.fizzBuzz(52));
+	}
+
+	@Test
+	public void fiftyThreeReturnsFizzBuzz()
+	{
+		assertEquals("fizz buzz", solution.fizzBuzz(53));
+	}
+
+	@Test
+	public void fiftyFiveReturnsBuzzFakeDeluxe()
+	{
+		assertEquals("buzz fake deluxe", solution.fizzBuzz(55));
+	}
+}
