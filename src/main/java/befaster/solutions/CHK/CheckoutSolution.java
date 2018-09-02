352a6c191b1fd22acefa64bb548154939656bce7
sbx_user1059 <sbx_user1059@ip-10-128-71-24.eu-west-2.compute.internal>
2018-09-02 15:23:26 +0000

    Sun Sep 02 15:23:26 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,17 +1,32 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CheckoutSolution
 {
+    private String[] alphabet = new String[]
+        {
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        };
+
     public Integer checkout(String skus)
     {
         if (!isValid(skus))
         {
             return -1;
         }
-        
+
+        Map<String, Integer> itemCounts = new HashMap<>();
+
+        for (String item: alphabet)
+        {
+            itemCounts.put(item, getCount(skus, item));
+        }
+
         int aCount = 0;
         int bCount = 0;
         int cCount = 0;
@@ -52,6 +67,13 @@ public class CheckoutSolution
     }
 
     private boolean isValid(String skus)
+    {
+        Pattern p = Pattern.compile("[A-Z]*");
+        Matcher m = p.matcher(skus);
+        return m.matches();
+    }
+
+    private int getCount(String skus, String item)
     {
         Pattern p = Pattern.compile("[A-Z]*");
         Matcher m = p.matcher(skus);
