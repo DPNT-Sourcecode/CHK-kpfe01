e90c8454d092b8df9556604fbc3966c282868e4f
sbx_user1059 <sbx_user1059@ip-10-128-71-24.eu-west-2.compute.internal>
2018-09-02 16:41:26 +0000

    Sun Sep 02 16:41:26 UTC 2018

---
+++ src/main/java/befaster/solutions/CHK/price/VPrice.java
@@ -0,0 +1,23 @@
+package befaster.solutions.CHK.price;
+
+import java.util.Map;
+
+public class APrice implements Price
+{
+	@Override
+	public int getPrice(Map<Character, Integer> itemCounts)
+	{
+		int price = 0;
+		int aCount = itemCounts.get('A');
+
+		price += (aCount / 5) * 200;
+		aCount = aCount % 5;
+
+		price += (aCount / 3) * 130;
+		aCount = aCount % 3;
+
+		price += aCount * 50;
+
+		return price;
+	}
+}
